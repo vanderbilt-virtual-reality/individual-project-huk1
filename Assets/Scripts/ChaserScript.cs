986d6c099143b83ccb2c8f076e7ea62559f6a74a
huk1 <kevin.hu@vanderbilt.edu>
2019-11-20 19:35:47 -0600

    Horror gameplay

---
+++ Assets/Scripts/ChaserScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserScript : MonoBehaviour
+{
+    public Transform target;
+    private bool withinRange= false;
+    public AudioSource warningSound;
+    private int warningCooldown = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+        if(warningCooldown > 0)
+        {
+            warningCooldown--;
+        }
+        if(target != null)
+        {
+            transform.LookAt(target);
+        }
+        float distToPlayer = Vector3.Distance(target.position, transform.position);
+
+        if(distToPlayer <= 30.0)
+        {
+            if (!withinRange && warningCooldown == 0)
+            {
+                //means that player just encountered the monster, 
+                warningSound.Play();
+                withinRange = true;
+                warningCooldown = 3000;
+
+            }
+            else
+            {
+                withinRange = false;
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Jumpscare.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscare : MonoBehaviour
+{
+    public bool Activated;
+    public GameObject jumpScareObj;
+    public Animation jumpScareAnim;
+    public AudioSource jumpScareAudio;
+    public BoxCollider Trigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(WaitJumpscare());
+    }
+    IEnumerator WaitJumpscare()
+    {
+        jumpScareAnim.Play();
+        jumpScareAudio.Play();
+        yield return new WaitForSeconds(0.5f);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = new Vector3(10.67f, 10, 6.38f);
+        GameObject chaser = GameObject.FindGameObjectWithTag("Monster");
+        chaser.transform.position = new Vector3(76.6f, 0.85f, 7.6f);
+        print("Success!");
+    }
+}
